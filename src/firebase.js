c9060c1a185bcad43b8b5494a16faa7a27645673
Meir Meir <lightir21@gmail.com>
2022-03-25 03:10:50 +0300

    settings

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyB1BNPuF4fcylUsYeGOoJmnsqL34DHwFk8",
-  authDomain: "findog-ce0ee.firebaseapp.com",
-  projectId: "findog-ce0ee",
-  storageBucket: "findog-ce0ee.appspot.com",
-  messagingSenderId: "175179629894",
-  appId: "1:175179629894:web:16580d88a9628e34c39147",
-  measurementId: "G-VL70Y49XBR",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 export default firebaseConfig;
